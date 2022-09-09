17664b4dbd467c8b4154c7ef3b05560191474dd6
sclarke0 <87775802+sclarke0@users.noreply.github.com>
2022-09-09 09:31:25 +0100

    Add files via upload

--- Cribis/cribis.py
+++ Cribis/cribis.py
@@ -27,145 +27,123 @@ ITALY_COUNTRY_CODE = 'IT'
 async def get_cribis_company(query: str, max_results: int = 100):
     client = zeep.Client(CRIBIS_API_WSDL_URL)
 
-    '''
-    Query can be in of the following formats, or a mix of these:
-        - a single company name
-        - a list of space-separated company namees
-        - a list of company names of the form (companyname1 OR companyname2)
-        - a company name enclosed in quotes (" or ')
-        - any of these with duplicates present (i.e. same company name mentioned more than once)
-    '''
-    query_items = []
-    for _query_item in query.replace('%20', ' ').split(' '):
-        _query_item = _query_item.strip(' ()"\'')
-        _should_add_query_item = (
-            _query_item and
-            (_query_item.lower() not in ['or',]) and
-            (_query_item not in query_items)
-        )
-        if _should_add_query_item:
-            query_items.append(_query_item)
 
     search_results = []
-    for queried_companies_count, company_name in enumerate(query_items):
-        try:
-            app_transaction_id = str(uuid.uuid4())
-            response = client.service.CompanySearch(
-                Username=CRIBIS_USERNAME,
-                Password=CRIBIS_PASSWORD,
-                ApplicationTransactionID=app_transaction_id,
-                CustomerReferenceText=str(uuid.uuid4()),
-                SearchData={
-                    'FlagActiveOnly': True,
-                    'FlagHQOnly': False,
-                    'MaximumHits': max_results,
-                    'CompanyName': company_name,
-                }
-            )
-        except:
-            if queried_companies_count > 0 and search_results:
-                # In case multiple companies are queried, and we error out after querying atleast one company,
-                # the return the search results we have so far instead of erroring out completely.
-                return {'searchResults': search_results}
-            return {'error': [{'message': 'Error querying the CRIBIS API.'}]}
-
-        try:
-            if response.TransactionResponse.Details.ApplicationTransactionID != app_transaction_id:
-                return {'error': [{'message': 'Received an illegal response from the CRIBIS API. %s' % response}]}
-        except AttributeError:
-            print(response)
-            return {'error': [{'message': 'Received an incomplete response from the CRIBIS API. %s' % response}]}
-
-        try:
-            if response.TransactionResponse.Result.Code not in ('OK', 'CS006',):
-                '''
-                Codes:
-                    OK      - Query is successful, and there are results present.
-                    CS006   - Query is successful, but no results found.
-                '''
-                return {'error': [{'message': 'Received an error response from the CRIBIS API. %s' % response}]}
-        except AttributeError:
-            print(response)
-            return {'error': [{'message': 'Received an incomplete result from the CRIBIS API. %s' % response}]}
-
-        try:
-            company_list = (response.CompanyList.CompanyItem or []) if response.CompanyList else []
-        except AttributeError:
-            print(response)
-            return {'error': [{'message': 'Received an incomplete company list from the CRIBIS API. %s' % response}]}
-        for company in company_list:
-            company_uuid = str(uuid.uuid3(uuid.NAMESPACE_DNS, company['CrifNumber']))
-            try:
-                last_balance_data_str = company['LastBalanceDate'].strftime('%d/%m/%Y')
-            except:
-                last_balance_data_str = '-'
-
-            description = (company['ActivityDescription'] or '').replace('\r', '').replace('\n', '\r\n')
-            if len(description) > 150:
-                description = f'{description[:150].strip()}...'
-
-            # Create search result
-            subtitle = f'Company Branch - {company["CrifNumber"]}'
-            if company['UnitTypeCode'] == 'S':
-                subtitle = f'Company Headquarters - {company["CrifNumber"]}'
-
-            result = {
-                'key': str(uuid.uuid4()).upper(),
-                'title': company['CompanyName'],
-                'subTitle': subtitle,
-                'summary': (
-                    f'Crif Number: {company["CrifNumber"] or "-"} | '
-                    f'VAT Number: {company["VATCode"] or "-"} | '
-                    f'Province: {company["ProvinceCode"] or "-"} | '
-                    f'Status: {company["ActivityStatusCodeDescription"] or "-"} | '
-                    f'Last Balance Date: {last_balance_data_str or "-"} | '
-                    f'Description: {description or "-"} | '
-                    f'Website: {company["WebSite"] or "-"}'
-                ),
-                'source': 'CRIBIS API',
-                'url': (
-                    f'https://www2.cribisx.com/#Purchase/CompanyByDUNS/{company["DunsNumber"]}'
-                    if company['DunsNumber'] else 'https://www2.cribisx.com'
-                ),
-                'entities': [
-                    {
-                        'id': company_uuid,
-                        'type': 'EntityBusiness',
-                        'attributes': {
-                            'Name': company['CompanyName'] or '',
-                            'LocalName': company['CompanyName'] or '',
-                            'CompanyNumber': company['CrifNumber'] or '',
-                            'VatNumber': company['VATCode'] or '',
-                            'Status': company["ActivityStatusCodeDescription"] or '',
-                            'Duns': company['DunsNumber'] or '',
-                            'RegistrationState': company['Region'] or '',
-                            'RegistrationCountry': ITALY_COUNTRY_CODE or '',
-                            'Liquidated': company['FlagOutOfBusiness'] is True,
-                            'TradeDescription': description or '',
-                            'StatusSince': last_balance_data_str or '',
-                        },
-                    },
-                ],
+    #for queried_companies_count, company_name in enumerate(query):
+    try:
+        app_transaction_id = str(uuid.uuid4())
+        response = client.service.CompanySearch(
+            Username=CRIBIS_USERNAME,
+            Password=CRIBIS_PASSWORD,
+            ApplicationTransactionID=app_transaction_id,
+            CustomerReferenceText=str(uuid.uuid4()),
+            SearchData={
+                'FlagActiveOnly': True,
+                'FlagHQOnly': False,
+                'MaximumHits': max_results,
+                'CompanyName': query,
             }
+        )
+    except:
+        return {'error': [{'message': 'Error querying the CRIBIS API.'}]}
 
-            if company['WebSite']:
-                webpage_uuid = str(uuid.uuid3(uuid.NAMESPACE_DNS, company['WebSite']))
-                result['entities'].append({
-                    'id': webpage_uuid,
-                    'type': 'EntityWebPage',
+    try:
+        if response.TransactionResponse.Details.ApplicationTransactionID != app_transaction_id:
+            return {'error': [{'message': 'Received an illegal response from the CRIBIS API. %s' % response}]}
+    except AttributeError:
+        print(response)
+        return {'error': [{'message': 'Received an incomplete response from the CRIBIS API. %s' % response}]}
+
+    try:
+        if response.TransactionResponse.Result.Code not in ('OK', 'CS006',):
+            '''
+            Codes:
+                OK      - Query is successful, and there are results present.
+                CS006   - Query is successful, but no results found.
+            '''
+            return {'error': [{'message': 'Received an error response from the CRIBIS API. %s' % response}]}
+    except AttributeError:
+        print(response)
+        return {'error': [{'message': 'Received an incomplete result from the CRIBIS API. %s' % response}]}
+
+    try:
+        company_list = (response.CompanyList.CompanyItem or []) if response.CompanyList else []
+    except AttributeError:
+        print(response)
+        return {'error': [{'message': 'Received an incomplete company list from the CRIBIS API. %s' % response}]}
+    for company in company_list:
+        company_uuid = str(uuid.uuid3(uuid.NAMESPACE_DNS, company['CrifNumber']))
+        try:
+            last_balance_data_str = company['LastBalanceDate'].strftime('%d/%m/%Y')
+        except:
+            last_balance_data_str = '-'
+
+        description = (company['ActivityDescription'] or '').replace('\r', '').replace('\n', '\r\n')
+        if len(description) > 150:
+            description = f'{description[:150].strip()}...'
+
+        # Create search result
+        subtitle = f'Company Branch - {company["CrifNumber"]}'
+        if company['UnitTypeCode'] == 'S':
+            subtitle = f'Company Headquarters - {company["CrifNumber"]}'
+
+        result = {
+            'key': str(uuid.uuid4()).upper(),
+            'title': company['CompanyName'],
+            'subTitle': subtitle,
+            'summary': (
+                f'Crif Number: {company["CrifNumber"] or "-"} | '
+                f'VAT Number: {company["VATCode"] or "-"} | '
+                f'Province: {company["ProvinceCode"] or "-"} | '
+                f'Status: {company["ActivityStatusCodeDescription"] or "-"} | '
+                f'Last Balance Date: {last_balance_data_str or "-"} | '
+                f'Description: {description or "-"} | '
+                f'Website: {company["WebSite"] or "-"}'
+            ),
+            'source': 'CRIBIS API',
+            'url': (
+                f'https://www2.cribisx.com/#Purchase/CompanyByDUNS/{company["DunsNumber"]}'
+                if company['DunsNumber'] else 'https://www2.cribisx.com'
+            ),
+            'entities': [
+                {
+                    'id': company_uuid,
+                    'type': 'EntityBusiness',
                     'attributes': {
-                        'Url': company['WebSite'],
+                        'Name': company['CompanyName'] or '',
+                        'LocalName': company['CompanyName'] or '',
+                        'CompanyNumber': company['CrifNumber'] or '',
+                        'VatNumber': company['VATCode'] or '',
+                        'Status': company["ActivityStatusCodeDescription"] or '',
+                        'Duns': company['DunsNumber'] or '',
+                        'RegistrationState': company['Region'] or '',
+                        'RegistrationCountry': ITALY_COUNTRY_CODE or '',
+                        'Liquidated': company['FlagOutOfBusiness'] is True,
+                        'TradeDescription': description or '',
+                        'StatusSince': last_balance_data_str or '',
                     },
-                })
-                result['entities'].append(create_relationship(company_uuid, webpage_uuid, 'Company Website'))
+                },
+            ],
+        }
 
-            search_results.append(result)
+        if company['WebSite']:
+            webpage_uuid = str(uuid.uuid3(uuid.NAMESPACE_DNS, company['WebSite']))
+            result['entities'].append({
+                'id': webpage_uuid,
+                'type': 'EntityWebPage',
+                'attributes': {
+                    'Url': company['WebSite'],
+                },
+            })
+            result['entities'].append(create_relationship(company_uuid, webpage_uuid, 'Company Website'))
 
-            if len(search_results) >= max_results:
-                break
+        search_results.append(result)
 
         if len(search_results) >= max_results:
             break
+
+    #if len(search_results) >= max_results:
+     #   break
 
     return {'searchResults': search_results}
 
